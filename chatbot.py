9d2ea559a44b370b0eac7285f6e6ccd3e3d9f5c0
pranavxiyer <pranavxiyer@gmail.com>
2023-08-19 13:01:29 -0700

    some intents

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,13 @@
+import numpy as np
+import pandas as pd
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+import tflearn
+import tensorflow
+import random
+import json
+
+stemmer = LancasterStemmer()
+
+print("test imports")
 
